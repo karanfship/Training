1bb530ec7f150a96677dc79970db02409bdcc29d
unknown <karanfship@gmail.com>
2022-05-01 16:57:21 +0530

    Updated the Hover effect for Navbar

--- reya-web-app6/src/components/Hero.jsx
+++ reya-web-app6/src/components/Hero.jsx
@@ -6,18 +6,18 @@ const Hero = () => {
     <div className='text-white'>
       <div className='max-w-[800px] mt-[-96px] w-full h-screen mx-auto text-center flex flex-col justify-center'>
         <p className='text-[#00df9a] font-bold p-2'>
-          GROWING WITH DATA ANALYTICS
+          SPINED WITH LOVE
         </p>
-        <h1 className='md:text-7xl sm:text-6xl text-4xl font-bold md:py-6'>
-          Grow with data.
+        <h1 className='md:text-5xl sm:text-4xl text-2xl font-bold md:py-6'>
+          WELCOME TO CROCHET WORLD.
         </h1>
         <div className='flex justify-center items-center'>
-          <p className='md:text-5xl sm:text-4xl text-xl font-bold py-4'>
-            Fast, flexible financing for
+          <p className='md:text-4xl sm:text-3xl text-xl font-bold py-4'>
+            Fast, cheap, reliable products for
           </p>
           <Typed
           className='md:text-5xl sm:text-4xl text-xl font-bold md:pl-4 text-gray-500 pl-2'
-            strings={['BTB', 'BTC', 'SASS']}
+            strings={['Purse', 'Bag', 'Cloth']}
             typeSpeed={120}
             backSpeed={140}
             loop
