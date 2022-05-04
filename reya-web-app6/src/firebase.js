55ce03ccc7a06e01f394683d0b49b6991f597010
karanfship <73127233+karanfship@users.noreply.github.com>
2022-05-04 19:54:03 +0530

    Update firebase.js

--- reya-web-app6/src/firebase.js
+++ reya-web-app6/src/firebase.js
@@ -1,21 +1,24 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCOhkL41FDkOnymZJtf3LbOp9_9eTvClZc",
-  authDomain: "reya-web-app6-bd232.firebaseapp.com",
-  projectId: "reya-web-app6-bd232",
-  storageBucket: "reya-web-app6-bd232.appspot.com",
-  messagingSenderId: "1048248542683",
-  appId: "1:1048248542683:web:1cced49fd1492ae9ccf3fe",
-  measurementId: "G-ZRNGXWYLDH"
+  apiKey: "AIzaSyAl5HBzmPadHumi1cjqSfcIMwaF434C_bw",
+  authDomain: "fir-project-335419.firebaseapp.com",
+  databaseURL: "https://firebaseproject-335419-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "firebaseproject-335419",
+  storageBucket: "firebaseproject-335419.appspot.com",
+  messagingSenderId: "951621100892",
+  appId: "1:951621100892:web:d751fab2e501a05296cd99"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+export { db, auth };
